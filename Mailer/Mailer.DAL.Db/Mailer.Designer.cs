1718af0a68a5e4b583b9465d63dd74434f77820b
Adam Mocarski <mocarski.contact@gmail.com>
2017-10-11 19:51:20 +0100

    Change projects .net version & upgrade libraries.

--- Mailer/Mailer.DAL.Db/Mailer.Designer.cs
+++ Mailer/Mailer.DAL.Db/Mailer.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Adam\Documents\Visual Studio 2017\Projects\Learning\Mailer\Mailer\MailerDb\Mailer.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Adam\Documents\Visual Studio 2017\Projects\Learning\Mailer\Mailer\Mailer.DAL.Db\Mailer.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
