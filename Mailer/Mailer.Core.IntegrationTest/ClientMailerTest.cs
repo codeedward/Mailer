27644295f5148520c187a0c119e9f4147d2c3012
Adam Mocarski <mocarski.contact@gmail.com>
2017-10-15 20:17:54 +0100

    Do simple integration test on db.

--- Mailer/Mailer.Core.IntegrationTest/ClientMailerTest.cs
+++ Mailer/Mailer.Core.IntegrationTest/ClientMailerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Mailer.Common.Models;
 using Mailer.Core.Models;
 using Mailer.DAL.Repository;
@@ -20,8 +21,34 @@ namespace Mailer.Core.IntegrationTest
                     new EmailQueueRepository()
                 )
             );
+
+            var response = clientMailer.Send(
+                new CoreEmailDto()
+                {
+                    EmailType = 2,
+                    TriesLeft = 5,
+                    AvailableToSendFromUtc = DateTime.UtcNow,
+                    From = new EmailRecipient("abc@email.com", "abcTestName"),
+                    SubjectTemplate = "Subject",
+                    BodyTemplate = "Body",
+                    Host = "123",
+                    Port = 25,
+                    To = new List<EmailRecipient>() { new EmailRecipient("abc@email.com", "abcTestName") }
+                });
+
+            Assert.IsTrue(response.MailIds.Count == 1);
+        }
+
+        [TestMethod]
+        public void SaveEmailAndRemoveTest()
+        {
+            var clientMailer = new ClientMailer(
+                new EmailQueueServiceCore(
+                    new EmailQueueRepository()
+                )
+            );
             
-            clientMailer.Send(
+            var response = clientMailer.Send(
                 new CoreEmailDto()
                 {
                     EmailType = 2,
@@ -34,6 +61,28 @@ namespace Mailer.Core.IntegrationTest
                     Port = 25,
                     To = new List<EmailRecipient>() {new EmailRecipient("abc@email.com", "abcTestName")}
                 });
+
+            Assert.IsTrue(response.MailIds.Count == 1);
+
+            using (var dbContext = new EmailQueueRepository().MailerContext)
+            {
+                var savedEmailId = response.MailIds[0];
+                var addedEmailQueue = dbContext.EmailQueues.Where(x => x.EmailQueueId == savedEmailId).ToList().FirstOrDefault();
+                var addedEmailMessage = dbContext.EmailMessages.Where(x => x.EmailMessageId == addedEmailQueue.EmailMessageId).ToList().FirstOrDefault();
+
+                Assert.IsNotNull(addedEmailQueue);
+                Assert.IsNotNull(addedEmailMessage);
+
+                dbContext.EmailQueues.Remove(addedEmailQueue);
+                dbContext.EmailMessages.Remove(addedEmailMessage);
+                dbContext.SaveChanges();
+
+                var removedEmailQueue = dbContext.EmailQueues.Where(x => x.EmailQueueId == savedEmailId).ToList().FirstOrDefault();
+                var removedEmailMessage = dbContext.EmailMessages.Where(x => x.EmailMessageId == addedEmailQueue.EmailMessageId).ToList().FirstOrDefault();
+
+                Assert.IsNull(removedEmailQueue);
+                Assert.IsNull(removedEmailMessage);
+            }
         }
     }
 }
